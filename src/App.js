1728f8e080ac73ee5e9fe257bb1016bab2419fb5
Muhammad Zain <zainsafdar934@gmail.com>
2026-02-09 17:01:31 +0500

    Initialize components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import { Box } from "@mui/material"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { ChannelDetail, Feed, Navbar, SearchFeed, VideoDetail } from "./components"
+
 
 const App = () => {
   return (
-    <div>
-      hello
-    </div>
+    <BrowserRouter>
+      <Box>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   )
 }
 
 export default App
+
