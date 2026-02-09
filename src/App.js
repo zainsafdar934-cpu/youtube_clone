b24d01718bc2080d5ef8cb5a5636569f9d1dc687
Muhammad Zain <zainsafdar934@gmail.com>
2026-02-09 19:52:35 +0500

    add SearchBar component and modify Navbar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import { Box } from "@mui/material"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { ChannelDetail, Feed, Navbar, SearchFeed, VideoDetail } from "./components"
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from "./components"
 
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Box>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Feed />} />
-          <Route path="/video/:id" element={<VideoDetail />} />
-          <Route path="/channel/:id" element={<ChannelDetail />} />
-          <Route path="/search/:searchTerm" element={<SearchFeed />} />
-        </Routes>
-      </Box>
-    </BrowserRouter>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
+
 
 export default App
 
