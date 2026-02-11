beb4b0d33a8b5ba24eb06039553061e9b7fc478c
Muhammad Zain <zainsafdar934@gmail.com>
2026-02-11 23:12:25 +0500

    Modify Videos component

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,17 @@
+import { Box, Stack } from "@mui/material";
 import React from "react";
 
 const Videos = ({ videos }) => {
-  console.log(videos);
-
-  return <div style={{ color: "white" }}>Videos Component</div>;
+  return (
+    <Stack direction={"row"} flexWrap={"wrap"} justifyContent={"start"} gap={2}>
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
